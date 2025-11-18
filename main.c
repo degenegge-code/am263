1c4d3e34883bf20efba26cd64311be371fdb5bf0
Jan Degenegge Prantner <degenegge@gmail.com>
2025-11-18 10:05:18 +0100

    first commit i hope

---
+++ main.c
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include "ti_drivers_config.h"
+#include "ti_board_config.h"
+
+// prototypyzde (?)
+void epwm_updown_main(void *args);
+
+int main(void)
+{
+    System_init();
+    Board_init();
+
+    epwm_updown_main(NULL);
+
+    Board_deinit();
+    System_deinit();
+
+    return 0;
+}
